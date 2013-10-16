bf77d07081d06b19022c79b191f161c9e7bd936a
John Yeh <jyeh@tribune.com>
2013-10-16 14:06:13 -0700

    new project from Xcode template - nothing added and nothing removed - initial check-in

---
+++ AudioFun/AudioFun/TDVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDVAppDelegate.h
+//  AudioFun
+//
+//  Created by John Yeh on 10/16/13.
+//  Copyright (c) 2013 Tribune Digital Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AudioFun/AudioFun/TDVViewController.m
@@ -0,0 +1,29 @@
+//
+//  TDVViewController.m
+//  AudioFun
+//
+//  Created by John Yeh on 10/16/13.
+//  Copyright (c) 2013 Tribune Digital Ventures. All rights reserved.
+//
+
+#import "TDVViewController.h"
+
+@interface TDVViewController ()
+
+@end
+
+@implementation TDVViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
